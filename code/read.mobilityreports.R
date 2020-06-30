882101014e34eb35d48034c17f2573a8f14b6e1c
sk608 <66416069+sk608@users.noreply.github.com>
2020-06-30 14:56:56 +0100

    renamed the downloader function, and added notes and removed unneccessary code lines, and added the metoffice data function script that downloads the metoffice data and forms a new table.

---
+++ code/read.mobilityreports.R
@@ -0,0 +1,14 @@
+
+read.mobilityreports<-function(){
+  #Defines the url where the mobility is taken from.
+  base.url<-"https://www.google.com/covid19/mobility/"
+  #Extracts data from the webpage at the defined url and maintains the other htmls information.
+  base.url2<-getURL(base.url)
+  parsed<-htmlParse(base.url2)
+  #Extracts all links that are present on this page.
+  doc.links<-xpathSApply(parsed,path = "//a",xmlGetAttr,"href")
+  #Extracts links that are only csvs'
+  csv.url<- as.character(doc.links[grep('csv', doc.links)])
+  #Downloads the recent data set
+  Google_Mobility_data<<-read.csv(csv.url, header = TRUE)
+}
