92ce6cdc296f73efbc52c9316ac1a3a0477f834e
AnnaPhD <64308370+AnnaPhD@users.noreply.github.com>
2020-07-03 19:51:55 +0100

    Adding Bedford map as default from function

--- app.R
+++ app.R
@@ -1,6 +1,9 @@
 ## app.R ##
 #need to make it a package 
 
+#libraries
+#-----------
+
 library(here)
 library(rgdal)
 library(leaflet)
@@ -13,65 +16,58 @@ library(dashboardthemes)
 library(htmlwidgets)
 library(htmltools)
 
-#Build the shiny App
-#source(here("/Parks_and_pandemic/get_spatial_data.R"))
+# Data
+#--------------------
 
+#source(here("/Parks_and_pandemic/get_spatial_data.R"))
+source(here("code/plot.googlemobilitydistricts.R"))
 bedford<-readOGR(dsn=paste0(here(),"/data/spatial"), layer="TL_GreenspaceSite")
 shapeData <- spTransform(bedford, CRS("+init=epsg:4326"))
 
+# Function definitions
+# --------------------
+
+# INSERT FUNCTION TO GET MAP DATA
+
+# UI
+# ------------------
+
 ui <- dashboardPage(
-  
-  dashboardHeader(
-    
-    title = "Parks in the Pandemic Dashboard",
-    
-    titleWidth = 400
-    
-  ),
-  
-  dashboardSidebar(out=h3("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-")),
-  
-  dashboardBody(tags$head(tags$style(HTML('.box {margin: 0px;}'))),
-    
-   fluidRow(
-     leafletOutput("map1")
-   ),
-  fluidRow(
-    box(dateRangeInput("daterange1", "Date range:",start = "2020-01-01",end   = "2020-08-31")),
-    box(plotOutput("plot1"))
-          )        
+        dashboardHeader(
+          title = "Parks in the Pandemic Dashboard",
+          titleWidth = 400),
+        dashboardSidebar(out=h3("Insert Text")),
+        dashboardBody(tags$head(tags$style(HTML('.box {margin: 0px;}'))),
+          fluidRow(
+            leafletOutput("map1")),
+          fluidRow(
+            box(dateRangeInput("daterange1", "Date range:",start = "2020-01-01",end   = "2020-08-31")),
+            box(plotOutput("plot1")))        
           )
         )
  
     
-
-  
-
-  
+# server
+#---------
 
 server <- function(input, output) {
+  
+    # MAKE MAP
+  
     #slow but wont be here in the final app - we can have a function to update the data and run the function here
     #need to change the path here
     UK_latlon <- readRDS(here("/data/UK_dat_ggplot.RDS"))
-  
-    
     UK_Mobility <- readRDS(here("/data/UK_Mobility.RDS"))
-    
-   
     UKdata<-reactive({
       UK_Mobility %>% 
-        filter(between(date,min(as.Date(input$daterange1)), max(as.Date(input$daterange1))))
-      
-    })
-  
+        filter(between(date,min(as.Date(input$daterange1)), max(as.Date(input$daterange1))))}
+    )
     #
     #output$mymap1<- renderLeaflet({ m@map }) 
       # leaflet(UK_latlon,width = "50%", height = 400) %>%
         #    addTiles() %>%
          #   addPolygons()
     #})
-    
     output$map1<-renderLeaflet({
       map <- leaflet()  %>% addTiles() %>% 
         setView(lng = -0.46, lat=52.13,zoom=12) %>% 
@@ -90,12 +86,10 @@ server <- function(input, output) {
     #     addPolygons(data=shapeData,weight=5,col = 'blue')
     #   map
     # })
-    output$plot1<-renderPlot({
-      UKdata() %>%
-        ggplot(aes(date, parks_percent_change_from_baseline))+
-        geom_point()
-    })
-
+    
+    # MAKE PLOT
+    output$plot1<-renderPlot({print(plot.googlemobilitydistricts(google,"parks","Bedford"))})
 }
 
+# Run the application
 shinyApp(ui, server)
