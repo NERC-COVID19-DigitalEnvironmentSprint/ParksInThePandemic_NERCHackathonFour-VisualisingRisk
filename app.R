c939cc8d0d61ceb3c4c5908126f6632c897f2c5a
Matthew Grainger <matthew.grainger@nina.no>
2020-07-09 15:52:29 +0200

    Add chloro

--- app.R
+++ app.R
@@ -42,17 +42,27 @@ if (file.exists("data/temporal/google_and_metoffice.csv")) {
 #UK_latlon <- readRDS("data/UK_dat_ggplot.RDS")
 #UK_Mobility <- readRDS("data/UK_Mobility.RDS")
 
-#import google boundaries shapefile from Open Science Framework data repository
-pp_project <- osf_retrieve_node("c7kg4")
-osf_ls_files(pp_project, pattern='WGS84') %>% osf_download(path='data/spatial')
-shapeData<-readOGR(dsn="data/spatial", layer="googleboundaries_WGS84")
+if (file.exists("data/spatial/googleboundaries_WGS84.shp")) {
+  shapeData<-readOGR(dsn="data/spatial", layer="googleboundaries_WGS84")
+} else {
+  #import google boundaries shapefile from Open Science Framework data repository
+  pp_project <- osf_retrieve_node("c7kg4")
+  osf_ls_files(pp_project, pattern='WGS84') %>% osf_download(path='data/spatial')
+  shapeData<-readOGR(dsn="data/spatial", layer="googleboundaries_WGS84")
+  
+}
+
 
 # Widgets
 # -------
 
-text.box <- box(title = "How busy is my local park likely to be?", footer = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit malesuada dolor, ac auctor mi venenatis nec. Suspendisse ipsum augue, luctus venenatis sagittis sit amet, posuere non velit. Praesent aliquam finibus consectetur. Phasellus laoreet nisi tincidunt lorem fringilla tincidunt. Fusce quis pulvinar ipsum, a blandit nisl. Sed quis est rhoncus, porta nunc a, cursus tellus. Morbi euismod erat felis. Sed varius quam vel eros congue, vel convallis justo ullamcorper. Maecenas posuere, justo sed dapibus posuere, leo ex dapibus est, id viverra neque odio in lorem. Proin lobortis ante est, sed aliquet orci varius sed.", width = 12)
+# this layout is very tidy 
+text.box <- box(title = "How busy are my local parks likely to be?", footer = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit malesuada dolor, ac auctor mi venenatis nec. Suspendisse ipsum augue, luctus venenatis sagittis sit amet, posuere non velit. Praesent aliquam finibus consectetur. Phasellus laoreet nisi tincidunt lorem fringilla tincidunt. Fusce quis pulvinar ipsum, a blandit nisl. Sed quis est rhoncus, porta nunc a, cursus tellus. Morbi euismod erat felis. Sed varius quam vel eros congue, vel convallis justo ullamcorper. Maecenas posuere, justo sed dapibus posuere, leo ex dapibus est, id viverra neque odio in lorem. Proin lobortis ante est, sed aliquet orci varius sed.", width = 12)
 
-date.box <- dateRangeInput("daterange1", "Date range:", start="2020-01-01", end="2020-08-31")
+date.box <- dateRangeInput("daterange1", "Date range:", start="2020-01-01", end="2021-01-01")
+
+place.box<-selectInput("place", "Choose a region", choices=unique(shapeData$NAME)
+                       , selected = "Bedford (B)", multiple = FALSE, selectize = TRUE, width = NULL, size = NULL)
 
 graph <- plotOutput("plot1")
 
@@ -66,7 +76,7 @@ map <- plotOutput("map1", height=700, width=400)
 header <- dashboardHeader(title="Parks in the Pandemic", titleWidth = 250)
 
 
-sidebar <- dashboardSidebar(date.box, width = 250)
+sidebar <- dashboardSidebar(date.box,place.box, width = 250)
 
 body <- dashboardBody(
   tags$head(tags$style(HTML('
@@ -98,13 +108,34 @@ body <- dashboardBody(
 server <- function(input, output) {
   google_react<-reactive({
     google %>% 
-      dplyr::filter(between(as.Date(date),min(as.Date(input$daterange1)), max(as.Date(input$daterange1))))}
-  )
+      dplyr::filter(between(as.Date(date),min(as.Date(input$daterange1)), max(as.Date(input$daterange1)))) %>% 
+      select(sub_region_1, date, parks_percent_change_from_baseline) %>% 
+      group_by(sub_region_1) %>% 
+      dplyr::summarise(mn=mean(parks_percent_change_from_baseline, na.rm=TRUE)) %>% 
+      dplyr::mutate(NAME=sub_region_1)
+    })
+  google_react2<-reactive({
+    google %>% 
+      dplyr::filter(between(as.Date(date),min(as.Date(input$daterange1)), max(as.Date(input$daterange1)))) 
+  })
   
-  output$map1<-renderPlot({
-    
-    par(mar=c(3, 0, 3, 0))
-    map <- plot(shapeData, xlim=c(-5.5,1.5), ylim=c(50,56))
+   shapeData2<-reactive({
+     shapeData2 <- shapeData[shapeData$NAME==input$place,]  
+     })
+     
+  
+  google_shp_merge<-reactive({merge(shapeData, google_react())})
+ 
+   output$map1<-renderPlot({
+      par(mar=c(3, 0, 3, 0))
+        mycolours <- RColorBrewer::brewer.pal(8, "Blues")
+        mybreaks <- c(-60,-40,-20,0, 20,40,60)
+        mycolours <- RColorBrewer::brewer.pal(8, "Blues")
+        mybreaks <- c(-60,-40,-20,0, 20,40,60)
+        #cut(google_shp_merge$mn, mybreaks)
+        mycolourscheme <- mycolours[findInterval(google_shp_merge()$mn, vec = mybreaks)]
+        plot(google_shp_merge(), xlim=c(-5.5,1.5), ylim=c(50,56), col = mycolourscheme)
+        plot(shapeData2(), xlim=c(-5.5,1.5), ylim=c(50,56), add=TRUE, density=2,lwd=4)
     
     })
   #map
@@ -115,7 +146,7 @@ server <- function(input, output) {
       #map
       #})
   #plot
-    output$plot1<-renderPlot({print(plot.googlemobilitydistricts(google_react(), "parks", "Bedford"))})
+    output$plot1<-renderPlot({print(plot.googlemobilitydistricts(google_react2(), "parks", "Bedford"))})
 }
   
 # Run
