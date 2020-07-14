b8dca765fe8a702bee8707d4b16c9166ac6eba42
sk608 <66416069+sk608@users.noreply.github.com>
2020-07-14 16:18:44 +0100

    Updates get forecast, to take the minimum and maximum of daily temperature, and also will extract the aggregated function for every district in England. Adds an RDS file to input_data for comparing the google regions to the shapefile names, and adds the colour aethetics and grid lines to the plot.googlemobilitydistrict function!

--- code/plot.googlemobilitydistricts.R
+++ code/plot.googlemobilitydistricts.R
@@ -1,4 +1,3 @@
-
 #DEFAULT SETTINGs TO PARKS and BEDFORD (Data stil needs to be unputted)
 
 plot.googlemobilitydistricts<-function(Data,type="parks",district="Bedford"){
@@ -39,28 +38,34 @@ plot.googlemobilitydistricts<-function(Data,type="parks",district="Bedford"){
   
   #Creates a character vector that resembles the column name within the google mobility data.
   Loc<-paste(type,"_percent_change_from_baseline",sep = "")
+  #Subsetting by colour
+  Data<-add_column(Data,mean_colour =  
+                     (ifelse(Data[,Loc] >= 0,"green","grey")))
   #Creates a smaller data set that only corresponds to the values needed.
-  district_data<-subset(Data,select = c("date","sub_region_1",Loc))
-  #Makes many data frames for each district.
-  district_all<-split(district_data,district_data$sub_region_1)
-  #Makes a data frame of the single district required.
-  district_df<-as.data.frame(district_all[district])
-  #Changes the column names for making it easier to put into a plotting function.
-  colnames(district_df)<-c("Date","District","Mobility")
+  district_data<-subset(Data,Data$sub_region_1 == district,select = c("date","sub_region_1",Loc,"mean_colour"))
+  
+  #Subset for positive values
+  Data_green<-subset(district_data, mean_colour == "green" )
+  #Subset for negative values
+  Data_grey<-subset(district_data, mean_colour == "grey")
   #Cleans the type title to ensure that _ do not exist in the y axis and recreates the y-axis.
   country_ylab<-paste("Visit changes for",gsub("_"," ",type),"(%) relative to per-weekday winter baselines \n(Google Community Mobility data)")
-  #START OF THE PLOTTING FUNCTION 
-  District_graph<-ggplot(data=district_df,aes(x=as.Date(Date),y=Mobility)) +
+  
+  #START OF THE PLOTTING FUNCTION
+  District_graph<-ggplot() +
     #Plots the bar graphs, with a black outing and dark orange fill. 
-    geom_col(position = position_dodge(width=0.2), size=0.25,colour = 'black', fill ='#D55E00') +
+    geom_col(Data_green, mapping = aes(x = date, y = parks_percent_change_from_baseline),position = position_dodge(width=0.2), size=0.25,colour = 'black', fill ='dark green') +
+    geom_col(Data_grey, mapping = aes (x = date, y = parks_percent_change_from_baseline), position = position_dodge(width = 0.2), size = 0.25, colour = 'black', fill = 'grey') +
     #Limits the size of the graph.
     coord_cartesian(ylim=c(-100,160)) +
     #plots a horizontal line where no percentage change occurs.
     geom_hline(yintercept=0) + 
     #Ensure the background is white, the border is black and removes grid lines.
     theme(panel.background = element_rect(fill = "white", colour = "black", size = 1, linetype = "solid"),
-          panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "grey"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "grey"),
           strip.text = element_blank())+
     #x-label
     xlab("Date") +
