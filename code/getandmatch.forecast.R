7c202f2355e99f9f62f211e5f8bf97940ac71f03
sk608 <66416069+sk608@users.noreply.github.com>
2020-07-15 10:53:13 +0100

    Fixes the forecasting error. (Hopefully) and adds comments for each peice of code.

--- code/getandmatch.forecast.R
+++ code/getandmatch.forecast.R
@@ -52,14 +52,16 @@ location_1<-ifelse(location == "Greater Manchester", location <- c("Bolton","Bur
                                                     ifelse(location == "Windsor and Maidenhead", location <- c("Windsor","Maidenhead"),
                    location)))))))
 
-#Creating a function
+#Get's the forecast and creates a tibble for the first location in the location vector.
 forecast <-get_forecast(location[1], units = "metric")%>%
   owmr_as_tibble()%>%
+#Then adds a column for it's designated sub_region_1.
   add_column(sub_region_1 = location[1],.after = "dt_txt")
 
-if(length(location) >= 1){
+#If the location vector is above 1, it adds the additional location data into the forecasting data frame.
+if(length(location) > 1){
          forecast<-forecast
-         for (i in 1:length(location)) {
+         for (i in 2:length(location)) {
             forecast_1<-get_forecast(location[i], units = "metric")%>%
                         owmr_as_tibble()%>%
                         add_column(sub_region_1= location[i], .after = "dt_txt")
@@ -71,26 +73,31 @@ if(length(location) >= 1){
 date_time<-t(as.data.frame(strsplit(forecast$dt_txt," ")))
 #Then combines them to the main data frame.
 forecast<-as.data.frame(cbind("date" = (as.Date(date_time[,1])), "time" = date_time[,2],forecast))[,-3]
-
+#Selects a subset of the forecast dataframe for easing transformations of the table.
 forecast<-subset(forecast, select = c("date","sub_region_1","temp","temp_min","temp_max","rain_3h"))
-
+#Converts the rain per 3 hours into rain per hour to be compatible with the metoffice.
 forecast<-forecast%>%
   mutate(
     rain_3h = rain_3h/3
   )
-
+#If location is not equal to the end location (i.e. it's a vector) then it will aggregate all the values
 ifelse (location != end_location,
       forecast_2<-aggregate(forecast[,3:length(forecast)], list(forecast$date,forecast$sub_region_1), FUN = mean, na.rm = T ),
       forecast_2<-forecast
       )
+#This renames the column names as the above code will change the column names if an aggregation has taken place.
 colnames(forecast_2)<-c("date","sub_region_1","temp","temp_min","temp_max","rain_3h")
 
-
+#This calculates the aggregation value for mean temperature by taking the mean.
 forecast_temp_mean<-aggregate(forecast_2$temp, list(forecast_2$date), FUN = mean, na.rm = T )
+#This calculates the max temperature by taking the max.
 forecast_temp_max<-aggregate(forecast_2$temp_max, list (forecast_2$date), FUN = max, na.rm = T)
+#This calculates the min temperature by taking the min.
 forecast_temp_min<-aggregate(forecast_2$temp_min, list (forecast_2$date), FUN = min, na.rm = T)
+#This calculates the aggregation value for the mean rainfall per hour by taking the mean.
 forecast_rain_mean<-aggregate(forecast_2$rain_3h, list (forecast_2$date), FUN = mean, na.rm = T)
 
+#This combines the above vectors created and adds weekdays for each value, date, and creates a column for sub_region_1 based from the first definition.
 forecast_2<-cbind.data.frame("weekdays" = weekdays(forecast_temp_mean$Group.1),
                   "date" = forecast_temp_mean$Group.1,
                   "sub_region_1" =end_location,           
