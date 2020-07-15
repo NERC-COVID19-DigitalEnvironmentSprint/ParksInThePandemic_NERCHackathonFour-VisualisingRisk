1b07e3a3e9494670291c65e2d37bf3eec173cb27
sk608 <66416069+sk608@users.noreply.github.com>
2020-07-15 14:33:18 +0100

    Ensure that each grey and green associated dataframe has the whole set of date data.

--- code/plot.googlemobilitydistricts.R
+++ code/plot.googlemobilitydistricts.R
@@ -1,5 +1,9 @@
 plot.googlemobilitydistricts<-function(google,type="parks",district="Bedford"){
   
+  #google = read.csv("~/GitHub/parksinthepandemic/code/input_data/testdata/google_england.csv")
+  #type = "parks"
+  #district = "Bedford"
+  
   # Load packages ----------------------------------------------------------
   #install.packages('tibble')
   library(tibble)
@@ -11,6 +15,8 @@ plot.googlemobilitydistricts<-function(google,type="parks",district="Bedford"){
   Loc<-paste(type,"_percent_change_from_baseline",sep = "")
   #Data
   Data<-google
+  Data[,"date"]<-as.Date(Data$date)
+  
   #Subsetting by colour
   Data<-tibble::add_column(Data,mean_colour =  
                      (ifelse(Data[,Loc] >= 0,"green","grey")))
@@ -21,14 +27,23 @@ plot.googlemobilitydistricts<-function(google,type="parks",district="Bedford"){
   Data_green<-subset(district_data, mean_colour == "green" )
   #Subset for negative values
   Data_grey<-subset(district_data, mean_colour == "grey")
+  
+  Data_not_grey<-cbind.data.frame("date" = Data_green$date,"sub_region_1" = Data_green$sub_region_1,"parks_percent_change_from_baseline" = 0, "mean_colour" = Data_green$mean_colour)
+  
+  Data_not_green<-cbind.data.frame("date" = Data_grey$date,"sub_region_1" = Data_grey$sub_region_1,"parks_percent_change_from_baseline" = 0, "mean_colour" = Data_grey$mean_colour)
+  
+  Data_green_all<-rbind(Data_green,Data_not_green)
+  
+  Data_grey_all<-rbind(Data_grey, Data_not_grey)
+  
   #Cleans the type title to ensure that _ do not exist in the y axis and recreates the y-axis.
   country_ylab<-paste("Visit changes for",gsub("_"," ",type),"(%) relative to per-weekday winter baselines \n(Google Community Mobility data)")
   
   #START OF THE PLOTTING FUNCTION
   District_graph<-ggplot2::ggplot() +
     #Plots the bar graphs, with a black outing and dark orange fill. 
-    geom_col(Data_green, mapping = aes(x = as.Date(date), y = parks_percent_change_from_baseline),position = position_dodge(width=0.2), size=0.25,colour = 'black', fill ='dark green') +
-    geom_col(Data_grey, mapping = aes (x = as.Date(date), y = parks_percent_change_from_baseline), position = position_dodge(width = 0.2), size = 0.25, colour = 'black', fill = 'grey') +
+    geom_col(Data_green_all, mapping = aes(x = as.Date(date), y = parks_percent_change_from_baseline),position = position_dodge(width=0.2), size=0.25,colour = 'black', fill ='dark green') +
+    geom_col(Data_grey_all, mapping = aes (x = as.Date(date), y = parks_percent_change_from_baseline), position = position_dodge(width = 0.2), size = 0.25, colour = 'black', fill = 'grey') +
     #Limits the size of the graph.
     coord_cartesian(ylim=c(-100,160)) +
     #plots a horizontal line where no percentage change occurs.
