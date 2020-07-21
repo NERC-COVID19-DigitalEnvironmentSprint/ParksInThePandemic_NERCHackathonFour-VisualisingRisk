0ef9cb70edc14445ca4798c9ceb983e9a2b707ba
sk608 <66416069+sk608@users.noreply.github.com>
2020-07-21 09:58:05 +0100

    Creates a model function, saves a model in input_data, alters code and adds datasets that make it compatible with the new model

--- code/examplepipeline.R
+++ code/examplepipeline.R
@@ -8,10 +8,12 @@ setwd('~/Github/parksinthepandemic/code')
 source('read.googlemobility.R')
 source('read.metofficecovid.R')
 source('read.naturalenglandmene.R')
+source('getandmatch.mene.R')
 
 source('match.metoffice.R')
 source('getandmatch.forecast.R')
 source('match.mene2google.R')
+source('match.OSgreenspace2google.R')
 
 source('relative2baseline.R')
 
@@ -27,7 +29,7 @@ met<-read.metofficecovid()
 write.csv(met, 'input_data/testdata/met.csv', row.names = F)
 
 #natural england MENE data
-mene<-read.naturalenglandmene()
+mene<-getandmatch.mene()
 write.csv(mene,'input_data/testdata/mene.csv', row.names=F)
 
 # MATCH DATASETS ----------------------------------------------------------
@@ -41,8 +43,22 @@ forecast_bedford<-getandmatch.forecast('Bedford',apikey="~/GitHub/apikey.RDS")
 write.csv(forecast_bedford,'input_data/testdata/forecast_bedford.csv', row.names=F)
 
 #match natural england mene data to google districts
-mene_england<-match.mene2google('input_data/testdata/mene.csv')
+mene<-read.menedata(relative2greenspace = F,relative2population = F)
+mene_population<-read.menedata(relative2population = T,relative2greenspace = F)
+mene_greenspace<-read.menedata(relative2greenspace = T,relative2population = F)
+mene_both<-read.menedata(relative2greenspace = T,relative2population = T)
+
+mene_england<-cbind.data.frame("sub_region_1" = mene$google_district,
+                    "estimated_average_annual_visit_count" = mene$estimated_average_annual_visit_count,
+                    "annual_visits_per_capita" = mene_population$annual_visits_per_capita,
+                    "annual_visits_per_km2_greenspace_1km_radius" = mene_greenspace$annual_visits_per_km2_greenspace_1km_radius,
+                    "annual_visits_per_capita_per_km2_greenspace_1km_radius" = mene_both$annual_visits_per_capita_per_km2_greenspace_1km_radius)
+
 write.csv(mene_england,'input_data/testdata/mene_england.csv', row.names=F)
+
+#Create garden_acess data frame and saves to testdata.
+garden_access<-match.OSgreenspace2google()
+write.csv(garden_access,'input_data/testdata/garden_access.csv', row.names=F)
 
 # MAKE WEATHER DATA RELATIVE TO BASELINE AND MERGE WITH GOOGLE ----------------------------------
 
