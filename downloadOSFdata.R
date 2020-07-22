698cc9beb3aed93e9607c62fafc436708d5e0005
Matt Lloyd Jones <befriendabacterium@users.noreply.github.com>
2020-07-22 12:12:38 +0100

    script just to download the OSF data to your local directory

---
+++ downloadOSFdata.R
@@ -0,0 +1,9 @@
+# Download data from OSF
+# ----------------------
+
+# Get tibble of data on the OSF store
+data <- osfr::osf_ls_files(osf_retrieve_node("c7kg4"))
+
+# Download to ./data
+dir.create("./data")
+osf_download(data, path="./data", verbose=TRUE, progress=TRUE, recurse=TRUE, conflicts="overwrite")
